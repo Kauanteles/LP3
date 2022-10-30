2528115231130e0433b53425888dbe73a7a1efea
Kauanteles <116807100+Kauanteles@users.noreply.github.com>
2022-10-30 20:07:58 -0300

    Add files via upload

---
+++ lista 2 (Contato)/contato.java
@@ -0,0 +1,61 @@
+public class contato {
+
+    private String nome;
+    private String email;
+    private int telefone;
+    private Date dateNascimento;
+
+    public void initializeContato(){
+
+    }
+    public void setContato(String nome, String email, int telefone, Date dateNascimento){
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.dateNascimento = dateNascimento;
+    }
+    
+    public String getNome(){
+        return nome;
+    }
+    public String getEmail(){
+        return email;
+    }
+    public int getTelefone(){
+        return telefone;
+    }
+    public int getDateNascimentoDay(){
+        return dateNascimento.getDay();
+    }
+    public int getDateNascimentoMonth(){
+        return dateNascimento.getMonth();
+    }
+    public int getDateNascimentoYear(){
+        return dateNascimento.getYear();
+    }
+    public void printContato(){
+         System.out.printf("\tContatoinfo:\nnome: %s\nemail: %s\ntelefone: %d\ndateNascimento: %d/%d/%d",nome,email,telefone,dateNascimento.getDay(),dateNascimento.getMonth(),dateNascimento.getYear());
+    }
+    public int ageCalculator(Date dateNascimento, Date dateAtual){
+        if(dateAtual.getYear() >= dateNascimento.getYear()){
+            if(dateAtual.getMonth() >= dateNascimento.getMonth()){
+                if(dateAtual.getDay() >= dateNascimento.getDay()){
+                    return(dateAtual.getYear() - dateNascimento.getYear());
+                }else {
+                    System.out.println("Erro: data de nascimento incompativel com data atual!");
+                    System.exit(-1);
+                    return 0;
+                }
+            }else {
+                System.out.println("Erro: data de nascimento incompativel com data atual!");
+                System.exit(-1);
+                return 0;
+            }
+        }else {
+            System.out.println("Erro: data de nascimento incompativel com data atual!");
+            System.exit(-1);
+            return 0;
+        }
+    }
+}
+
---
+++ lista 2 (Data)/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Date date = new Date();
+
+        date.setDate(30, 10, 2022);
+        date.printDate();
+        date.printFullDate();
+    }
+}
