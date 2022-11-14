fe102e76bc412aa6cc8454d2fdff682b36e7ec93
Kauanteles <116807100+Kauanteles@users.noreply.github.com>
2022-11-14 14:32:04 -0300

    Add files via upload

---
+++ lista 3 (Contato)/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        contato contato = new contato();
+        Date dateNascimento = new Date();
+        Date dateAtual = new Date();
+
+        dateNascimento.setDate(05, 02, 2004);
+        dateAtual.setDate(30, 10, 2022);
+
+        contato.setContato("kakau", "kakau@gmail.com", 991446395, dateNascimento);
+        
+        contato.printContato();
+        System.out.println("\n"+contato.ageCalculator (dateNascimento,dateAtual));
+    }
+}
