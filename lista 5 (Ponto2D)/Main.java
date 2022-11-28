c4cb812b43fda6f5d9b1141a558ff5b3244c7509
Kauan Souza Teles <116807100+Kauanteles@users.noreply.github.com>
2022-11-28 18:38:33 -0300

    Add files via upload

---
+++ lista 5 (Ponto2D)/Main.java
@@ -0,0 +1,26 @@
+public class Main{
+    public static void main(String[] args) {
+        Ponto2D ponto = new Ponto2D(0, 0);
+        Ponto2D ponto1 = new Ponto2D(100, 200);
+
+        System.out.println(ponto.getX());
+        System.out.println(ponto.getY());
+
+        ponto.imprimirPonto();
+
+        if(ponto.isEixoX()){
+            System.out.println("esta no eixo x");
+        }
+        if(ponto.isEixoY()){
+            System.out.println("esta no eixo y");
+        }
+
+        System.out.println(Ponto2D.quadrante(ponto));
+
+        System.out.println(Ponto2D.distancia(ponto,ponto1));
+        System.out.println(Ponto2D.distancia(ponto1));
+        System.out.println(ponto.distancia(ponto1.getX(),ponto1.getY()));
+
+        
+    }
+}
