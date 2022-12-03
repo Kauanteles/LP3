52e43039d3bf2cb591ef4690e3fc411400c58b87
Kauan Souza Teles <116807100+Kauanteles@users.noreply.github.com>
2022-12-02 21:42:50 -0300

    Delete lista 6 (B Ponto2D) directory

--- lista 6 (B Ponto2D)/Main.java
+++
@@ -1,9 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        Ponto2D ponto1 = new Ponto2D(1,2);
-        Ponto2D ponto2 = new Ponto2D(2,2);
-        SegmentoReta s = new SegmentoReta(ponto1,ponto2);
-
-        System.out.println(s.imprimeSegmentoReta());
-    }
-}
